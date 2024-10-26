e024cc3a4b45be91f16a0a22fd998f53cbfb678c
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-10-26 20:33:39 +0100

    Started working on some dropdown functionality for choosing particular models available on the given API Key.

--- static/toggle_sidebar.mjs
+++ static/toggle_sidebar.mjs
@@ -2,6 +2,10 @@ var sidebar_active = false;
 
 
 function sidebarToggle() {
-    document.getElementById("sidebar").setAttribute("style", `width: ${sidebar_active ? 0 : 18}%;`);
     sidebar_active = !sidebar_active;
+
+    document.getElementById("sidebar").setAttribute("style", `width: ${sidebar_active ? 18 : 0}%;`);
+    document.getElementById("sidebar_closer").setAttribute("style", `opacity: ${sidebar_active ? 1 : 0};`);
+    document.getElementById("sidebar_opener").setAttribute("style", `opacity: ${sidebar_active ? 0 : 1};`);
+    document.getElementById("modelSelector").setAttribute("style", `left: ${sidebar_active ? 1 : 5}%;`);
 }
