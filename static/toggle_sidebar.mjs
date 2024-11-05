bebf13bb280f085adf61cc5654072021d81eef54
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-11-05 16:08:44 +0000

    Fixed options bar, now smooth and scalable- if needed.

--- static/toggle_sidebar.mjs
+++ static/toggle_sidebar.mjs
@@ -7,5 +7,5 @@ function sidebarToggle() {
     document.getElementById("sidebar").setAttribute("style", `width: ${sidebar_active ? 18 : 0}%;`);
     document.getElementById("sidebar_closer").setAttribute("style", `opacity: ${sidebar_active ? 1 : 0};`);
     document.getElementById("sidebar_opener").setAttribute("style", `opacity: ${sidebar_active ? 0 : 1};`);
-    document.getElementById("modelSelector").setAttribute("style", `left: ${sidebar_active ? 1 : 5}%;`);
+    document.getElementById("modelSelector").setAttribute("style", `right: ${sidebar_active ? 60 : 0}%;`);
 }
