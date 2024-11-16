a77bfbdde6503f9af2b23583ef0a4790ff249865
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-11-16 15:19:30 +0000

    Adding another option to choose from.

--- static/toggle_sidebar.mjs
+++ static/toggle_sidebar.mjs
@@ -21,13 +21,14 @@ function sidebarToggle() {
  * @param {Number} sidebar_width The % width that the sidebar will take up on the screen.
  * @param {Number} closer_opacity The opacity of the sidebar closer button on this given toggle.
  * @param {Number} opener_opacity The opacity of the sidebar opener button on this given toggle.
- * @param {Number} model_sel_right How much the modelDropdown will move to the right by, (should be the column gap property of the <ul> tag).
+ * @param {Number} selector_right How much the modelDropdown and TuningDropdown will move to the right by, (should be the column gap property of the <ul> tag).
  * @param {String} opener_vis Whether the sidebar opener will collapse or be visible.
  */
-function setElementAttrs(sidebar_width, closer_opacity, opener_opacity, model_sel_right, opener_vis) {
+function setElementAttrs(sidebar_width, closer_opacity, opener_opacity, selector_right, opener_vis) {
     document.getElementById("sidebar").setAttribute("style", `width: ${sidebar_width}%;`);
     document.getElementById("sidebar_closer").setAttribute("style", `opacity: ${closer_opacity};`);
     document.getElementById("sidebar_opener").setAttribute("style", `opacity: ${opener_opacity};`);
-    document.getElementById("modelSelector").setAttribute("style", `right: ${model_sel_right}px;`);  // move right by the column gap amount
+    document.getElementById("modelSelector").setAttribute("style", `right: ${selector_right}px;`);  // move right by the column gap amount
+    document.getElementById("modelTuningSelector").setAttribute("style", `right: ${selector_right}px;`);  // move right by the column gap amount
     document.getElementById("sidebar_opener").parentElement.setAttribute("style", `visibility: ${opener_vis};`);  // remove the sidebar opener from the layout if necessary.
 }
