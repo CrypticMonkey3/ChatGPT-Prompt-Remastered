015e34c3fe62425899a1842910ec97bfe393e239
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-11-25 21:41:53 +0000

    Final polishing.

--- static/responseStylePrompt.mjs
+++ static/responseStylePrompt.mjs
@@ -23,12 +23,20 @@ function checkResponseStyleArea(set_style = "") {
 
 
 async function applyResponseStyle() {
+    let response_style_textarea = document.getElementById("responseStyleInput");
+
     await bodiedFetch(
         "/update-tuning-parameters",
         {
             "parameter": "response style",
-            "value": document.getElementById("responseStyleInput").value
+            "value": response_style_textarea.value
         }
     )
+
     document.getElementById("responseStyleConfirmation").classList.add("hide");
+    current_response_style = response_style_textarea.value;
+
+    if (response_style_textarea.value === "") {  // if the user's new style is nothing, then set the placeholder to that of the default style.
+        response_style_textarea.placeholder = "You are a helpful assistant.";
+    }
 }
